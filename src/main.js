608f90d169cd0bd4da8f6896a13a8fe8a13d78bb
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-06-03 23:54:41 -0700

    got the monsters to chase the player

--- src/main.js
+++ src/main.js
@@ -18,16 +18,46 @@ const config = {
   };
   
   let player, cursors;
+  const directions = ['down', 'up', 'left', 'right'];
   
-  
+  let golem1;
+  let bird1;
+  let monsters = [];
+  let monsterTypes = ['golem1', 'bird1', 'cacto']; // Array of monster sprite keys
+
   function preload() {
     this.load.image('Dungeon_Tileset', './src/assets/Dungeon_Tileset.png');
     this.load.tilemapTiledJSON('map', './src/assets/consoleTest.json');
-    this.load.atlas('bluechar', './src/assets/sprites/bluechar/bluechar-0.png', './src/assets/sprites/bluechar/bluechar.json')
-    this.load.atlas('golem1', './src/assets/monsters/golem1.png', './src/assets/monsters/golem1.json')
-    this.load.atlas('bird1', './src/assets/monsters/bird1.png', './src/assets/monsters/bird1.json')
+    loadSpriteAtlas(this, 'bluechar');
+    loadSpriteAtlas(this, 'golem1');
+    loadSpriteAtlas(this, 'bird1');
+    loadSpriteAtlas(this, 'cacto');
   
-    
+    function loadSpriteAtlas(context, spriteName) {
+      context.load.atlas(spriteName, `./src/assets/sprites/${spriteName}/${spriteName}.png`, `./src/assets/sprites/${spriteName}/${spriteName}.json`);
+    }
+  }
+  function chasePlayer(monster, speed) {
+    const direction = new Phaser.Math.Vector2(player.x - monster.x, player.y - monster.y);
+    direction.normalize();
+    monster.setVelocity(direction.x * speed, direction.y * speed);
+  
+    // Determine the direction of the monster
+    if (Math.abs(direction.x) > Math.abs(direction.y)) {
+      // Monster is moving more horizontally
+      if (direction.x > 0) {
+        monster.play(`${monster.texture.key}-right`, true);
+      } else {
+        monster.play(`${monster.texture.key}-left`, true);
+      }
+    } else {
+      // Monster is moving more vertically
+      if (direction.y > 0) {
+        monster.play(`${monster.texture.key}-down`, true);
+      } else {
+        monster.play(`${monster.texture.key}-up`, true);
+      }
+    }
   }
   
   function create() {
@@ -39,144 +69,33 @@ const config = {
     const overheadLayer = map.createLayer('Overhead', tileset);
     backgroundLayer.setCollisionByProperty({ collide: true });
     interactiveLayer.setCollisionByProperty({ collide: true });
-    const bluechar = this.add.sprite(128, 128, 'bluechar', 'down1.png' )
-    const golem1 = this.add.sprite(128, 156, 'golem1', 'down1.png' )
-    const bird1 = this.add.sprite(128, 128, 'bird1', 'down1.png' )
-   
-    this.anims.create({
-        key: 'bluechar-down',
-        frames: this.anims.generateFrameNames('bluechar', {
-            prefix: 'down',
-            suffix: '.png',
-            start: 1,
-            end: 3,
-        }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'bluechar-up',
-        frames: this.anims.generateFrameNames('bluechar', {
-            prefix: 'up',
-            suffix: '.png',
-            start: 1,
-            end: 3,
-        }),
-        frameRate: 10,
-        repeat: -1
-    }); 
-    this.anims.create({
-        key: 'bluechar-left',
-        frames: this.anims.generateFrameNames('bluechar', {
-            prefix: 'left',
-            suffix: '.png',
-            start: 1,
-            end: 3,
-        }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'bluechar-right',
-        frames: this.anims.generateFrameNames('bluechar', {
-            prefix: 'right',
-            suffix: '.png',
-            start: 1,
-            end: 3,
-        }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-      key: 'golem1-down',
-      frames: this.anims.generateFrameNames('golem1', {
-          prefix: 'down',
+    golem1 = this.physics.add.sprite(128, 180, 'golem1', 'down1.png');
+    cacto = this.physics.add.sprite(128, 180, 'cacto', 'down1.png');
+bird1 = this.physics.add.sprite(128, 128, 'bird1', 'down1.png');
+
+
+    directions.forEach(direction => createAnimationForCharacter(this, 'bluechar', direction));
+    directions.forEach(direction => createAnimationForCharacter(this, 'golem1', direction));
+    directions.forEach(direction => createAnimationForCharacter(this, 'bird1', direction));
+    directions.forEach(direction => createAnimationForCharacter(this, 'cacto', direction));
+  
+    function createAnimationForCharacter(context, character, direction) {
+      context.anims.create({
+        key: `${character}-${direction}`,
+        frames: context.anims.generateFrameNames(character, {
+          prefix: direction,
           suffix: '.png',
           start: 1,
           end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1
-  });
-  this.anims.create({
-      key: 'golem1-up',
-      frames: this.anims.generateFrameNames('golem1', {
-          prefix: 'up',
-          suffix: '.png',
-          start: 1,
-          end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1
-  }); 
-  this.anims.create({
-      key: 'golem1-left',
-      frames: this.anims.generateFrameNames('golem1', {
-          prefix: 'left',
-          suffix: '.png',
-          start: 1,
-          end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1
-  });
-  this.anims.create({
-      key: 'golem1-right',
-      frames: this.anims.generateFrameNames('golem1', {
-          prefix: 'right',
-          suffix: '.png',
-          start: 1,
-          end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1
-  });
-  this.anims.create({
-    key: 'bird1-down',
-    frames: this.anims.generateFrameNames('bird1', {
-        prefix: 'down',
-        suffix: '.png',
-        start: 1,
-        end: 3,
-    }),
-    frameRate: 10,
-    repeat: -1
-});
-this.anims.create({
-    key: 'bird1-up',
-    frames: this.anims.generateFrameNames('bird1', {
-        prefix: 'up',
-        suffix: '.png',
-        start: 1,
-        end: 3,
-    }),
-    frameRate: 10,
-    repeat: -1
-}); 
-this.anims.create({
-    key: 'bird1-left',
-    frames: this.anims.generateFrameNames('bird1', {
-        prefix: 'left',
-        suffix: '.png',
-        start: 1,
-        end: 3,
-    }),
-    frameRate: 10,
-    repeat: -1
-});
-this.anims.create({
-    key: 'bird1-right',
-    frames: this.anims.generateFrameNames('bird1', {
-        prefix: 'right',
-        suffix: '.png',
-        start: 1,
-        end: 3,
-    }),
-    frameRate: 10,
-    repeat: -1
-});
-    
-    bird1.anims.play('bird1-down');    golem1.anims.play('golem1-down');
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+    }
+  
+  // bird1.anims.play('bird1-down');
+  // golem1.anims.play('golem1-up');
+  // cacto.anims.play('cacto-up');
 
     player = this.physics.add.sprite(100, 450, 'bluechar');
     player.body.setSize(player.width * 0.4, player.height * 0.4); // Adjust the hitbox size here
@@ -184,16 +103,47 @@ this.anims.create({
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, backgroundLayer);  
     this.physics.add.collider(player, interactiveLayer); 
+    this.physics.add.collider(golem1, backgroundLayer);  
+    this.physics.add.collider(bird1, interactiveLayer); 
 
     cursors = this.input.keyboard.addKeys('W,A,S,D'); // add WASD keys here
    
     this.cameras.main.setZoom(2);
     this.cameras.main.startFollow(player);
+    for (let i = 0; i < 10; i++) {
+      let monsterType = Phaser.Utils.Array.GetRandom(monsterTypes); // Randomly select a monster sprite
   
+      const monster = this.physics.add.sprite(
+        Phaser.Math.Between(100, 700), // random X position between 100 and 700
+        Phaser.Math.Between(100, 500), // random Y position between 100 and 500
+        monsterType, // use the randomly selected monster sprite
+        'down1.png' // replace with your animation key
+      );
+  
+      monster.play(`${monsterType}-down`); // Play the corresponding animation for the monster
+      this.physics.add.collider(monster, backgroundLayer);
+      this.physics.add.collider(monster, interactiveLayer);
+  
+      // Add collision between the monster and the player
+      this.physics.add.collider(monster, player);
+      this.physics.add.collider(monster, golem1);
+this.physics.add.collider(monster, bird1);
+this.physics.add.collider(monster, cacto);
+
+      monsters.push(monster);
+    }
   
   }
   
   function update() {
+    chasePlayer(golem1, 150);
+    chasePlayer(cacto, 150);
+    chasePlayer(bird1, /* `80` is the speed at which the `bird1` sprite is chasing the player in the
+    `chasePlayer` function. */
+    50);
+    monsters.forEach((monster) => {
+      chasePlayer(monster, 50);
+    });
     // Horizontal movement
     if (cursors.A.isDown) {
         player.setVelocityX(-200);
